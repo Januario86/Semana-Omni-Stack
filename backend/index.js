d2016354c12dad2307aaaf6c2ef6c6083aa58174
felipe Januario <felipejanuario86@gmail.com>
2020-03-25 21:22:58 -0300

    listando usuários na aplicação

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(request,response) =>{
+/*
+Rotas / Recurso 
+*/
+/**
+ * Método HTTP:
+ * 
+ * GET:  buscar um informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: alterar uma informação no back-end
+ * DELETE: deletar uma informação no back-end
+ */
+app.get('/users',(request,response) =>{
 
     return response.json({
         evento:"Semana OmniStack 11.0",
